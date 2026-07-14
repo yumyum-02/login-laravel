5f16b3936a60a57e4e33133538f0c5e92aefc5cd
yumyum-02 <yumyum0kome27@gmail.com>
2026-07-14 21:35:17 +0900

    css呼び出しの修正でサイドバー表示

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
   <title>ダッシュボード</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
-  <link href="{{ asset('/css/style')}}" rel="stylesheet">
+  <link href="{{ asset('/css/style.css')}}" rel="stylesheet">
 </head>
 
 <body class="bg-light min-vh-100">
