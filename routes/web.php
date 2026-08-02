b43b80e75d0ab9b7a1cbfd074b0042af7902db53
yumyum-02 <yumyum0kome27@gmail.com>
2026-08-02 16:57:15 +0900

    メールアドレス変更/ルートの修正でメールアドレス変更画面を表示

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,13 @@ Route::get('edit-username' , function(){
     $user = Auth::user();
     return view('edit-username',['user' => $user]);
 })->name('edit-username')->middleware('auth');
-
-// ユーザー情報変更処理
 // ユーザー名変更 update
 Route::post('edit-username' , [EditUsernameController::class, 'update'])->name('update-username')->middleware('auth');
+
+// メールアドレス変更画面表示
+Route::get('edit-email' , function(){
+    $user = Auth::user();
+    return view('edit-email',['user' => $user]);
+})->name('edit-email')->middleware('auth');
 // メールアドレス変更 update
 Route::post('edit-email' , [EditEmailController::class, 'update'])->name('update-email')->middleware('auth');
